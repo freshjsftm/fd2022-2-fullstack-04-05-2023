b02dd7ae903ac46caab361d7c184792204aeaf6e
Olena Zdanovska <freshjs.ftm@gmail.com>
2023-05-08 16:07:30 +0300

    Add lazy & Suspense

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,44 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import UsersPage from './pages/UsersPage';
-import LogUpPage from './pages/LogUpPage';
-import GroupsPage from './pages/GroupsPage';
-import UserProfile from './components/UserProfile';
+// import HomePage from './pages/HomePage';
+// import UsersPage from './pages/UsersPage';
+// import LogUpPage from './pages/LogUpPage';
+// import GroupsPage from './pages/GroupsPage';
+// import UserProfile from './components/UserProfile';
+const HomePage = lazy(() => import('./pages/HomePage'));
+const UsersPage = lazy(() => import('./pages/UsersPage'));
+const LogUpPage = lazy(() => import('./pages/LogUpPage'));
+const GroupsPage = lazy(() => import('./pages/GroupsPage'));
+const UserProfile = lazy(() => import('./components/UserProfile'));
 
 const App = () => {
   return (
     <BrowserRouter>
       <nav>
         <ol>
-          <li><NavLink to="/">Home</NavLink></li>
-          <li><NavLink to="/users">Users</NavLink></li>
-          <li><NavLink to="/group-create">group create</NavLink></li>
-          <li><NavLink to="/registration">registration</NavLink></li>
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/users">Users</NavLink>
+          </li>
+          <li>
+            <NavLink to="/group-create">group create</NavLink>
+          </li>
+          <li>
+            <NavLink to="/registration">registration</NavLink>
+          </li>
         </ol>
       </nav>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/users" element={<UsersPage />} />
-        <Route path="/group-create" element={<GroupsPage />} />
-        <Route path="/registration" element={<LogUpPage />} />
-        <Route path="/users/:idUser" element={<UserProfile />} />
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/users" element={<UsersPage />} />
+          <Route path="/group-create" element={<GroupsPage />} />
+          <Route path="/registration" element={<LogUpPage />} />
+          <Route path="/users/:idUser" element={<UserProfile />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 };
